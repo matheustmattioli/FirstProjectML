7ef8f07d024361ea1bfe70c117ad9d8757f4f869
OakBranches.M <50147318+OakBranches@users.noreply.github.com>
2021-11-10 14:47:51 -0300

    impl pre-proc categorical attr
    Foi implementado o pré-processamento dos atributos categóricos.
    Utilizamos Live-Share para que todos colaborassem
    
    Co-Authored-By: Gabriel Penajo <55462743+gabrielpenajo@users.noreply.github.com>
    Co-Authored-By: Matheus Mattioli <51218353+matheustmattioli@users.noreply.github.com>

--- formula-python.py
+++ formula-python.py
@@ -5,6 +5,10 @@
 
 import numpy as np
 import pandas as pd
+from sklearn import preprocessing
+from sklearn.impute import SimpleImputer
+
+
 # import matplotlib.pyplot as plt
 
 
@@ -21,6 +25,36 @@ if __name__ == '__main__':
         "peak-rpm", "city-mpg", "highway-mpg", "price"
     ]
 
-    DataFrame = pd.read_csv(
+    # dataframe
+    df = pd.read_csv(
         'imports-85.data', header=None, names=column_headers)
-    print(DataFrame)
+        
+
+    # pré-processamento
+
+    # Atribuindo o valor NaN para informações ausentes
+    imp_const = SimpleImputer(missing_values='?', strategy='constant', fill_value=np.nan)
+    df = pd.DataFrame(imp_const.fit_transform(df), columns=column_headers)
+
+    # Convertendo atributos categóricos para atributos numéricos
+    enc = preprocessing.OrdinalEncoder()
+    categorical_columns = [
+        "make", "fuel-type", "aspiration",  "num-of-doors","body-style",
+        "drive-wheels", "engine-location", "engine-type", "num-of-cylinders",
+        "fuel-system"
+    ]
+    
+    df[categorical_columns] = enc.fit_transform(df[categorical_columns])
+
+    # Dividindo os casos de teste e casos de treino
+    # X = assim
+    # y = assado
+    # X_train, X_test, y_train, y_test = train_test_split(
+    #     X, y, test_size=0.33, random_state=42)
+
+
+    # Existem alguns dados faltando, portanto, vamos tratar isso
+    # imp_mean = SimpleImputer(missing_values='?', strategy='most_frequent')
+    # imp_mean.fit(df)
+
+    print(df)
