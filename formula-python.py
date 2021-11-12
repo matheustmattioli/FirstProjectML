16a337af6bb29f60bf5287ae97edcf36f7960ddc
Matheus Mattioli <matheus.mattioli@hotmail.com>
2021-11-11 22:18:50 -0300

    impl knn validation
    Implementamos o algoritmo knn para nosso conjunto de dados, escolhendo os melhores parâmetros através de Grid Search e argumentando essa escolha através da análise de métricas. Por fim, plotamos gráficos para melhor visualização.
    
    Co-Authored-By: OakBranches.M <50147318+OakBranches@users.noreply.github.com>
    Co-Authored-By: Gabriel Penajo <55462743+gabrielpenajo@users.noreply.github.com>

--- formula-python.py
+++ formula-python.py
@@ -11,7 +11,11 @@ from sklearn import preprocessing
 from sklearn.metrics import max_error
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.impute import SimpleImputer
-
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import cross_val_score
 
 # import matplotlib.pyplot as plt
 
@@ -21,9 +25,11 @@ DEBUG = 0
 scaler = preprocessing.StandardScaler()
 
 # Recebe um DataFrame e retorna um Dataframe normalizado
+
+
 def normalize_data(dataframe):
     dataframe = scaler.fit_transform(dataframe)
-    
+
     return dataframe
 
 
@@ -82,7 +88,7 @@ if __name__ == '__main__':
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.33, random_state=42)
-    
+
     # Existem alguns dados faltando, portanto, vamos tratar isso
     # Atribuir valores aos NaN
     imp_mean = SimpleImputer(missing_values=np.nan, strategy='median')
@@ -90,28 +96,94 @@ if __name__ == '__main__':
     X_train = pd.DataFrame(imp_mean.fit_transform(
         X_train), columns=column_headers_X)
     y_train = pd.DataFrame(imp_mean.fit_transform(
-            y_train), columns=column_headers_y)
+        y_train), columns=column_headers_y)
 
     X_test = pd.DataFrame(imp_mean.fit_transform(
         X_test), columns=column_headers_X)
     y_test = pd.DataFrame(imp_mean.fit_transform(
-            y_test), columns=column_headers_y)
+        y_test), columns=column_headers_y)
 
     # Converte os atributos categóricos para int
     X_train[categorical_columns] = X_train[categorical_columns].astype(int)
     X_test[categorical_columns] = X_test[categorical_columns].astype(int)
-    
+
     debug(f"{X_train}\n=====\n{y_train}")
 
+    # Vamos aplicar o GridSearch para buscar os hiperparametros ótimos para o
+    # algoritmo de regressão
+    param_grid = {'weights': ['uniform', 'distance'],
+                  'n_neighbors': range(1, 15, 2),
+                  'metric': ['euclidean', 'manhattan']}
+    knr = KNeighborsRegressor()
+    gs = GridSearchCV(knr, param_grid=param_grid)
+    gs.fit(X_train, y_train)
+    # Análise qualitativa do desempenho do melhor classificador
+    print(f'Melhor score obtido: {gs.best_score_}')
+    # Atributos do melhor classificador
+    print(f'Parametros do melhor classificador:\n{gs.best_estimator_}')
+    print(f'Parametros detalhados do melhor classificador:\n{gs.best_params_}')
 
-    erros = []
+    # Teste do classificador utilizando os melhores parâmetros
+    knr2 = KNeighborsRegressor(
+        metric='manhattan', n_neighbors=5, weights='distance')
+    knr2.fit(X_train, y_train)
+    y_best_pred = knr2.predict(X_test)
+    y_pred = y_best_pred
+    # Scores variados para avaliar o desempenho
+    print(f'R2-score: {r2_score(y_test, y_pred)}')
+    print(f'MSE: {mean_squared_error(y_test, y_pred)}')
+    print(f'MAE: {mean_absolute_error(y_test, y_pred)}')
 
-    for i in range(1,20):
-        neigh = KNeighborsRegressor(n_neighbors=i)
+    # 5-Fold Cross Validation
+    scores = cross_val_score(knr2, X_train, y_train, cv=5)
+    print(scores)
+    print(f"{scores.mean():.2f} acuracia com desvio padrao de {scores.std():.2f}")
+
+    # Análise do KNN para k no intervalo [1, 20]
+    errors_mae = []
+    errors_mse = []
+    errors_r2 = []
+    score_neigh = []
+    std_neigh = []
+
+    for i in range(1, 20, 2):
+        neigh = KNeighborsRegressor(
+            metric='manhattan', n_neighbors=i, weights='distance')
         neigh.fit(X_train, y_train)
 
         y_pred = neigh.predict(X_test)
-        erros.append(max_error(y_test, y_pred))
-    
-    plt.plot(range(1,20), erros)
+        errors_mae.append(mean_absolute_error(y_test, y_pred))
+        errors_mse.append(mean_squared_error(y_test, y_pred))
+        errors_r2.append(r2_score(y_test, y_pred))
+        scores = cross_val_score(neigh, X_train, y_train, cv=5)
+        score_neigh.append(scores.mean())
+        std_neigh.append(scores.std())
+    # Plot dos valores de cada métrica para melhor análise do resultado
+    plt.plot(range(1, 20, 2), errors_mae, label='Erro absoluto medio')
+    plt.plot(range(1, 20, 2), errors_mse, label='Erro quadratico medio')
+    plt.plot(range(1, 20, 2), errors_r2, label="R2")
+    plt.legend()
+    plt.xticks(np.arange(1, 21, 2))
+    plt.show()
+    # Plot dos scores e dos desvios padrões para o knn com k variando entre [1, 20]
+    plt.errorbar(x=range(1, 20, 2), y=score_neigh, yerr=std_neigh)
+    plt.xticks(np.arange(1, 21, 2))
+    plt.show()
+
+    # Plot do melhor resultado
+
+    y_test_ordered = y_test['price']
+    y_best_pred_ordered = y_best_pred
+
+    # Ordenando para melhorar a visualização
+    zipped_pairs = zip(y_best_pred_ordered, y_test_ordered)
+    tuple_list = list(zip(*sorted(zipped_pairs)))
+    y_best_pred_ordered, y_test_ordered = tuple_list[0], tuple_list[1]
+
+    plt.scatter(range(len(y_test_ordered)), y_test_ordered,
+             label='Preco real', color='#00FF00')
+    plt.plot(range(len(y_best_pred_ordered)),
+                y_best_pred_ordered, label='Preco predito', color='red')
+
+    plt.legend()
     plt.show()
